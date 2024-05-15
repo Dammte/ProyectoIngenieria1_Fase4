da078d629fff9646da72714198261182fa5b0e85
Daniel Jimenez <jhoanndaniel.2004@gmail.com>
2024-05-15 15:19:48 -0500

    Se añade Contact section y optimizacion

--- script.js
+++ script.js
@@ -28,3 +28,25 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollBtn").style.display = "block";
+    } else {
+        document.getElementById("scrollBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    let scrollToTop = window.setInterval(function() {
+        let pos = window.pageYOffset;
+        if (pos > 0) {
+            window.scrollTo(0, pos - 20);
+        } else {
+            clearInterval(scrollToTop);
+        }
+    }, 5); 
+}
+
